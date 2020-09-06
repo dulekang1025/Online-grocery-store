5dabbbc78e95a03eefe756ddda0d4e65d2ed2adc
Lekang <lekangdu@gmail.com>
2020-09-06 15:59:04 -0400

    fix bugs.

--- src/main/java/com/pika/Onlinegrocerystore/Services/ServiceImpl/CartServiceImpl.java
+++ src/main/java/com/pika/Onlinegrocerystore/Services/ServiceImpl/CartServiceImpl.java
@@ -1,32 +1,32 @@
-//package com.pika.Onlinegrocerystore.Services.ServiceImpl;
-//
-//import com.pika.Onlinegrocerystore.dao.ICartDao;
-//import com.pika.Onlinegrocerystore.dao.ICustomerDao;
-//import com.pika.Onlinegrocerystore.dao.IOrderDao;
-//import com.pika.Onlinegrocerystore.dao.IProductDao;
-//import com.pika.Onlinegrocerystore.domain.Cart;
-//import com.pika.Onlinegrocerystore.domain.Customer;
-//import com.pika.Onlinegrocerystore.domain.Product;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class CartServiceImpl {
-//    private final IProductDao productDao;
-//    private final ICartDao cartDao;
-//    private Double totalPrice;
-//
-//    @Autowired
-//    public CartServiceImpl(IProductDao productDao, ICartDao cartDao) {
-//        this.productDao = productDao;
-//        this.cartDao = cartDao;
+package com.pika.Onlinegrocerystore.Services.ServiceImpl;
+
+import com.pika.Onlinegrocerystore.dao.ICartDao;
+import com.pika.Onlinegrocerystore.dao.ICustomerDao;
+import com.pika.Onlinegrocerystore.dao.IOrderDao;
+import com.pika.Onlinegrocerystore.dao.IProductDao;
+import com.pika.Onlinegrocerystore.domain.Cart;
+import com.pika.Onlinegrocerystore.domain.Customer;
+import com.pika.Onlinegrocerystore.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartServiceImpl {
+    private final IProductDao productDao;
+    private final ICartDao cartDao;
+    private Double totalPrice;
+
+    @Autowired
+    public CartServiceImpl(IProductDao productDao, ICartDao cartDao) {
+        this.productDao = productDao;
+        this.cartDao = cartDao;
+    }
+
+//    @Override
+//    public void addProductById(Long id) {
+//        productDao.findProductById();
 //    }
-//
-////    @Override
-////    public void addProductById(Long id) {
-////        productDao.findProductById();
-////    }
-//
-//
-//
-//}
+
+
+
+}
